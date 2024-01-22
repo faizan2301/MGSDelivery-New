92d5f9937c965afb9c96cd97133a91df78f035ec
faizan2301 <skfaizan2301@gmail.com>
2024-01-22 15:41:26 +0530

    ok

--- src/helper/formatDate.js
+++ src/helper/formatDate.js
@@ -1,21 +1,30 @@
-
 export const formatDate = (date) => {
-  if (typeof (date) === "string") {
-    
+  if (typeof date === "string") {
+    if (date === undefined) return "date";
 
-    if (date === undefined) return "date"
-    const year = date.split("T")[0].replace('"', " ").split("-")[0]
-    const month = date.split("T")[0].replace('"', " ").split("-")[1]
-    const day = date.split("T")[0].replace('"', " ").split("-")[2]
-    return `${day}/${month}/${year}`
-  } else if (typeof (date) === "object") {
-    const getDate = new Date(date)
-    const newDate = `${getDate.getDate()}/${getDate.getMonth()}/${getDate.getFullYear()}`
-    return newDate
+    const year = date.split("T")[0].replace('"', " ").split("-")[0];
+    const month = date.split("T")[0].replace('"', " ").split("-")[1];
+    const day = date.split("T")[0].replace('"', " ").split("-")[2];
+    return `${day}/${month}/${year}`;
+  } else if (typeof date === "object") {
+    const getDate = new Date(date);
+    const newDate = `${getDate.getDate()}/${getDate.getMonth()}/${getDate.getFullYear()}`;
+    return newDate;
   } else {
-    return `No date found`
+    return `No date found`;
   }
-
-
+};
+export function getStartOfDay() {
+  const now = new Date();
+  const startOfDay = new Date(now);
+  startOfDay.setHours(0, 0, 0, 0);
+  return startOfDay;
 }
 
+// Function to get end of the day
+export function getEndOfDay() {
+  const now = new Date();
+  const endOfDay = new Date(now);
+  endOfDay.setHours(23, 59, 59, 999);
+  return endOfDay;
+}
